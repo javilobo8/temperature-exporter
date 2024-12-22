d9517e50b823ecbdabd91b2fd7ce45bfba6f8aa9
javilobo8 <jbermudezlobo@gmail.com>
2024-12-22 12:38:49 +0100

    add hostname

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -20,10 +21,16 @@ func HandleCollect(gauge prometheus.Gauge) {
 }
 
 func main() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	temperatures := prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "sensor_temperatures",
 		Help: "Sensor temperatures",
-	}, []string{"sensor"})
+	}, []string{"hostname", "sensor"})
 
 	registry := prometheus.NewRegistry()
 
@@ -33,7 +40,7 @@ func main() {
 		for {
 			s, _ := sensors.SensorsTemperatures()
 			for _, v := range s {
-				temperatures.WithLabelValues(v.SensorKey).Set(v.Temperature)
+				temperatures.WithLabelValues(hostname, v.SensorKey).Set(v.Temperature)
 			}
 			time.Sleep(30 * time.Second)
 		}
@@ -49,5 +56,5 @@ func main() {
 	)
 
 	// To test: curl -H 'Accept: application/openmetrics-text' localhost:8080/metrics
-	log.Fatalln(http.ListenAndServe(":8080", nil))
+	log.Fatalln(http.ListenAndServe(":9101", nil))
 }
